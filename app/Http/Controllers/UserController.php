04690cbf87631d1523b3d081c886ec07b38093e2
titicuevas <enrique.cuevas@iesdonana.org>
2021-12-10 14:23:24 +0100

    Creo bien el tailwind

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GrahamCampbell\ResultType\Success;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function loginForm()
+    {
+        return view('login');
+    }
+
+    public function login()
+    {
+        $validados = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $usuario = DB::table('users')->where('email', $validados['email']);
+
+        if ($usuario->exists()) {
+            $usuario = ($usuario->get())[0];
+
+            if (password_verify($validados['password'], $usuario->password)) {
+                session(['usuario' => $validados['email']]);
+                return redirect('/')->with('success', 'El usuario ha iniciado sesión.');
+            }
+        }
+
+        return redirect()->back()->with('error', 'Usuario o contraseña incorrectos.');
+    }
+
+    public function logout()
+    {
+        session()->forget('usuario');
+        return redirect()->back()->with('success', 'has hecho logout correctamente');
+    }
+
+    public static function logueado()
+    {
+        return request()->session()->has('usuario');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', [UserController::class, 'loginForm']);
+Route::post('login', [UserController::class, 'login']);
+Route::post('logout', [UserController::class, 'logout']);
